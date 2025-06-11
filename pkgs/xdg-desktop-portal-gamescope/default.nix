f31df4cb6b2eeef6cf0113edb687297be72a69df
K900 <me@0upti.me>
2025-06-11 10:33:11 +0300

    xdg-desktop-portal-gamescope: 0.1.20 -> 0.1.21

--- pkgs/xdg-desktop-portal-gamescope/default.nix
+++ pkgs/xdg-desktop-portal-gamescope/default.nix
@@ -13,18 +13,18 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "xdg-desktop-portal-gamescope";
-  version = "0.1.20.c54d973";
+  version = "0.1.21.20ac820";
 
   src = fetchFromGitHub {
     owner = "Jovian-Experiments";
     repo = "xdg-desktop-portal-gamescope";
-    rev = "c54d9735bed8c35f1d507fc1176fd01cd8b2416f";
-    hash = "sha256-6uuayxq/bZDaCBDeEcKbHQgDwwOTMgpW8chepaHi80I=";
+    rev = "20ac8204acbb0a9735e9b5de8fa814e95beabfe3";
+    hash = "sha256-GrBb9WfFYUTUxOGPbHYjde47psZ29KmgJM6mpfR4Kqo=";
   };
 
   cargoDeps = rustPlatform.fetchCargoVendor {
     inherit (finalAttrs) src;
-    hash = "sha256-siYdXhnR32OAaDRm/4OWFmlIlS8gYhZKjX+ddzrPVKw=";
+    hash = "sha256-sPm6ZXgQWCnTW6X2de85tjt5WZZcxpAOEOeSav6vmBA=";
   };
 
   env.PKG_CONFIG_DBUS_1_SESSION_BUS_SERVICES_DIR = "${placeholder "out"}/share/dbus-1/services";
