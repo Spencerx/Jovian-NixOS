22d9b06acf756b8c03254ad4303347e890e22923
K900 <me@0upti.me>
2025-10-29 11:48:25 +0300

    mangohud: 0.8.1.r103-1 -> 0.8.1.r103-2

--- pkgs/mangohud/default.nix
+++ pkgs/mangohud/default.nix
@@ -1,5 +1,5 @@
-{ mangohud', fetchFromGitHub }:
-mangohud'.overrideAttrs {
+{ mangohud', fetchFromGitHub, fetchpatch }:
+mangohud'.overrideAttrs(old: {
   version = "0.8.1.r103";
 
   src = fetchFromGitHub {
@@ -8,4 +8,12 @@ mangohud'.overrideAttrs {
     rev = "10479eb59aa6c1c92af9d2afca3e33c5a17d875b";
     hash = "sha256-9mylciDKXCYXHvAWco8ALLOeojGF5/h8Lp+c1dQfINg=";
   };
-}
+
+  # cherry-picked in jupiter-main
+  patches = old.patches or [] ++ [
+    (fetchpatch {
+      url = "https://github.com/flightlessmango/MangoHud/commit/ff6e5954ea7e340d9bb4509754babf0db1b7c431.diff";
+      hash = "sha256-PWMw0hOtUBzfezdDue3s4h+BNsNQvZmKbWLZfn9xO18=";
+    })
+  ];
+})
