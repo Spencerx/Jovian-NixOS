879adb19975452f5cf1f64d9988e0fdc27e70876
K900 <me@0upti.me>
2025-11-15 16:56:04 +0300

    mangohud: 0.8.2.rc1.r4 -> 0.8.2.rc1.r5

--- pkgs/mangohud/default.nix
+++ pkgs/mangohud/default.nix
@@ -1,11 +1,11 @@
 { mangohud', fetchFromGitHub }:
 mangohud'.overrideAttrs(old: {
-  version = "0.8.2.rc1.r4";
+  version = "0.8.2.rc1.r5";
 
   src = fetchFromGitHub {
     owner = "flightlessmango";
     repo = "mangohud";
-    rev = "1c0e8525bf105646b0ac2d002d13e481a4b6f270";
-    hash = "sha256-sHYy555wJfZSdhtMOC5UtfLf6gPGeuxSKDX8HH3ZOw8=";
+    rev = "3a2885dd91fa53eefd0f164919edccdd82942310";
+    hash = "sha256-RTeWaelTx3V+5Tg7S2qZc3A9xR1yvsTqzgoR91+p6N8=";
   };
 })
