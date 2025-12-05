22e18c8c11670d2765e295392359bc7a07943a7a
K900 <me@0upti.me>
2025-12-05 11:19:48 +0300

    mesa-radv-jupiter: 26.0.0-steamos-25.11.2 -> 26.0.0-steamos-25.11.3

--- pkgs/mesa-radv-jupiter/default.nix
+++ pkgs/mesa-radv-jupiter/default.nix
@@ -1,7 +1,7 @@
 { stdenv, mesa, fetchFromGitHub }:
 let
   version = "26.0.0";
-  jupiterVersion = "steamos-25.11.2";
+  jupiterVersion = "steamos-25.11.3";
 in stdenv.mkDerivation {
   pname = "mesa";
   version = "${version}.${jupiterVersion}";
@@ -10,7 +10,7 @@ in stdenv.mkDerivation {
     owner = "Jovian-Experiments";
     repo = "mesa";
     rev = jupiterVersion;
-    hash = "sha256-v+ADoaLU0HWQym6k2klIv5USgTDJYs/0srJAUxgx1+A=";
+    hash = "sha256-M7Me6yJmRsuaIEmybun6Haw8dXrfdp+ppAYltXhRAzM=";
   };
 
   inherit (mesa) buildInputs nativeBuildInputs propagatedBuildInputs;
