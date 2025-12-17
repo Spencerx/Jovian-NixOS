d201410f7080f4882d24e34fb04e7e378af36b36
K900 <me@0upti.me>
2025-12-17 10:46:31 +0300

    wireplumber-jupiter: 0.5.10-jupiter1.12 -> 0.5.10-jupiter1.13

--- pkgs/wireplumber/default.nix
+++ pkgs/wireplumber/default.nix
@@ -1,11 +1,11 @@
 { wireplumber', fetchFromGitHub }:
 wireplumber'.overrideAttrs(_: {
-  version = "0.5.10-1.12";
+  version = "0.5.10-1.13";
 
   src = fetchFromGitHub {
     owner = "Jovian-Experiments";
     repo = "wireplumber";
-    rev = "0.5.10-jupiter1.12";
-    hash = "sha256-DmEu2ojSkf+8OY/SQm6kbuUgHM1GufPVZTjlZPiNpj0=";
+    rev = "0.5.10-jupiter1.13";
+    hash = "sha256-F+r2vNWYu9chwDruzjomM/Jqfb18JBEqD0QKGlh7Qrk=";
   };
 })
