6208e8b78c4a5d768fbb8640b726a2d23bb971e8
K900 <me@0upti.me>
2026-01-24 12:20:48 +0300

    mangohud: 0.8.2.rc1.r5 -> 0.8.3.rc1.r13

--- pkgs/mangohud/default.nix
+++ pkgs/mangohud/default.nix
@@ -1,11 +1,34 @@
-{ mangohud', fetchFromGitHub }:
-mangohud'.overrideAttrs(old: {
-  version = "0.8.2.rc1.r5";
+{ mangohud', fetchFromGitHub, stdenv, meson, git }:
+let
+  # This is kind of a horrible hack to allow wraps to work.
+  # We may want this in nixpkgs? idk
+  version = "0.8.3.rc1.r13";
 
   src = fetchFromGitHub {
     owner = "flightlessmango";
     repo = "mangohud";
-    rev = "3a2885dd91fa53eefd0f164919edccdd82942310";
-    hash = "sha256-RTeWaelTx3V+5Tg7S2qZc3A9xR1yvsTqzgoR91+p6N8=";
+    rev = "756ffcfdd7a688ecef5716891e84b43cb9c9cb15";
+    hash = "sha256-ZhcJVhYXRTTouhOHB89QEwG4w7fYEdvigB3Q3zwNIJg=";
   };
+
+  mesonDeps = stdenv.mkDerivation {
+    pname = "mangohud-meson-deps";
+    inherit version src;
+
+    nativeBuildInputs = [ meson git ];
+
+    buildCommand = ''
+      runPhase unpackPhase
+      MESON_PACKAGE_CACHE_DIR=$out meson subprojects download
+    '';
+
+    outputHash = "sha256-5Cy68E6yB5ZUkIpQbphrTgaphA0vZQ8Ue3aDrvGeehw=";
+    outputHashAlgo = "sha256";
+    outputHashMode = "recursive";
+  };
+in
+mangohud'.overrideAttrs(old: {
+  inherit version src;
+
+  env.MESON_PACKAGE_CACHE_DIR = mesonDeps;
 })
