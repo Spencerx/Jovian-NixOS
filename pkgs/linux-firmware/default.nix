faf829408344456d53c33df0a99f2f307498c830
K900 <me@0upti.me>
2026-02-18 21:42:36 +0300

    linux-firmware-jupiter: 20251210.1 -> 20260216.1

--- pkgs/linux-firmware/default.nix
+++ pkgs/linux-firmware/default.nix
@@ -1,12 +1,12 @@
 { linux-firmware, fetchFromGitHub }:
 
 linux-firmware.overrideAttrs(_: rec {
-  version = "20251210.1";
+  version = "20260216.1";
 
   src = fetchFromGitHub {
     owner = "Jovian-Experiments";
     repo = "linux-firmware";
     rev = "jupiter-${version}";
-    hash = "sha256-+FXVUFJHfcLliyJmw9ovRevDMtPF9g2UBiVvTSBL5To=";
+    hash = "sha256-0O46ZVW0rRn6wztyB8SJb6WxFd6pnak0Ri1YxkBSqyY=";
   };
 })
