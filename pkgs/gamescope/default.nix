33c848d79a5496e704ca808a1b91529eae1406cc
K900 <me@0upti.me>
2026-02-25 10:42:21 +0300

    gamescope{,-session}: 3.16.19 -> 3.16.20

--- pkgs/gamescope/default.nix
+++ pkgs/gamescope/default.nix
@@ -7,13 +7,13 @@
 #       version shipped by the vendor, ensuring feature level is equivalent.
 
 gamescope'.overrideAttrs(old: rec {
-  version = "3.16.19";
+  version = "3.16.20";
 
   src = fetchFromGitHub {
     owner = "ValveSoftware";
     repo = "gamescope";
     rev = version;
     fetchSubmodules = true;
-    hash = "sha256-Bwj781MVeQexjYnHfDArgjVjl7eQW+CzbdKrLdcAKkg=";
+    hash = "sha256-o/mzamXrKxhxlGlYy9zW2VfVh4gHL/eNw4PuK138WYc=";
   };
 })
