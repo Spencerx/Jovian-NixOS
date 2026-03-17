7a3543517498be1bd8aa11cbdf96dfa9a256432d
K900 <me@0upti.me>
2026-03-17 10:31:19 +0300

    mangohud: 0.8.3.rc1.r18 -> 0.8.3.rc1.r24

--- pkgs/mangohud/default.nix
+++ pkgs/mangohud/default.nix
@@ -2,13 +2,13 @@
 let
   # This is kind of a horrible hack to allow wraps to work.
   # We may want this in nixpkgs? idk
-  version = "0.8.3.rc1.r18";
+  version = "0.8.3.rc1.r24";
 
   src = fetchFromGitHub {
     owner = "flightlessmango";
     repo = "mangohud";
-    rev = "01b724d3e43f668babcbd70b550944bea4bdd524";
-    hash = "sha256-aGrHjcEzir9vq+bbhY1lLX1qmrNMtasUnoKbd3HGAS0=";
+    rev = "33c2c7ddbb72c15e19a42163d75424d5804f8ec8";
+    hash = "sha256-sbu3Ms8cy4/s1N/JkF3UVR3/3mGk1OAed1AfTM2j1iE=";
   };
 
   mesonDeps = stdenv.mkDerivation {
@@ -22,7 +22,7 @@ let
       MESON_PACKAGE_CACHE_DIR=$out meson subprojects download
     '';
 
-    outputHash = "sha256-5Cy68E6yB5ZUkIpQbphrTgaphA0vZQ8Ue3aDrvGeehw=";
+    outputHash = "sha256-LIIAbvFmFgVVuGoTbxNVSdKlpmOH+JOEJQxEGIjitf4=";
     outputHashAlgo = "sha256";
     outputHashMode = "recursive";
   };
