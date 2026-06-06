bcd4ed35efbd070376f22ba3d190be8a97ccc85b
K900 <me@0upti.me>
2026-06-06 10:54:45 +0300

    vpower: 1.5.7 -> 1.6.0

--- pkgs/vpower/default.nix
+++ pkgs/vpower/default.nix
@@ -6,13 +6,13 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "vpower";
-  version = "1.5.7";
+  version = "1.6.0";
 
   src = fetchFromGitHub {
     owner = "Jovian-Experiments";
     repo = "vpower";
     tag = finalAttrs.version;
-    hash = "sha256-GmDyi/PSuxWsCAs3mVCBJu/6dEzTlkgGKXzBe4r+xvk=";
+    hash = "sha256-3gk/pt8rgx1WjFWmgA023TjhyxHrwNwyDpgYmqFOsKQ=";
   };
 
   postPatch = ''
@@ -20,7 +20,7 @@ rustPlatform.buildRustPackage (finalAttrs: {
       --replace-fail /usr/lib/vpower $out/bin/vpower
   '';
 
-  cargoHash = "sha256-mk02dEOYY1FWOU/77UomxZH6s6NZieO6j8v7/MXV6MQ=";
+  cargoHash = "sha256-66K8ODzcBWhypXqEAvSnFgxMJAwFybsGLM3ANc0xSko=";
 
   buildInputs = [
     lm_sensors
