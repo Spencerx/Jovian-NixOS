6313d960912f51e9b93e8e8040b47fd46d6bed5e
K900 <me@0upti.me>
2026-06-10 09:41:33 +0300

    inputplumber: 0.77.2 -> 0.77.4

--- pkgs/inputplumber/default.nix
+++ pkgs/inputplumber/default.nix
@@ -4,17 +4,17 @@
   rustPlatform,
 }:
 inputplumber'.overrideAttrs rec {
-  version = "0.77.2";
+  version = "0.77.4";
 
   src = fetchFromGitHub {
     owner = "ShadowBlip";
     repo = "InputPlumber";
     tag = "v${version}";
-    hash = "sha256-QxGDmuRTZLHBqZ+S73CtNKdLg/XumutGSpTSz4sGFYQ=";
+    hash = "sha256-7yN89FEgQg+KeBrlCPbWIL8FE7abikdff9alZ9FMkqU=";
   };
 
   cargoDeps = rustPlatform.fetchCargoVendor {
     inherit src;
-    hash = "sha256-EtCK/zYRW2LdZhPSrO6ydBXBQwIrkqaJmH+3XfwERTw=";
+    hash = "sha256-BLIjerf5SYKhc3FB0lR3iWaD5+KXh3XmWy1ujwl8fVU=";
   };
 }
